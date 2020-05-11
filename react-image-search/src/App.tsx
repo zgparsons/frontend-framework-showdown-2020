1aa6c12419ed09b03aa4c62d8d783e4c39b76679
zgparsons <z.g.parsons@outlook.com>
2020-05-11 20:33:18 +0100

    Functionality for React Image Search

--- react-image-search/src/App.tsx
+++ react-image-search/src/App.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import getImages from './api';
 
 const App = () => {
+  const [title] = useState('React Image Search');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [images, setImages] = useState<string[]>([]);
+
+const formSubmitted = async (event: React.SyntheticEvent<EventTarget>): Promise<void> => {
+  event.preventDefault();
+  setImages([]);
+  setLoading(true);
+  const images = await getImages(searchTerm);
+  setImages(images);
+  setLoading(false);
+}
+
+const onSearchTermChanged = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  setSearchTerm(event.target.value);
+}
+
   return (
       <React.Fragment>
-        <h1>React JS Image Search</h1>
-        <form>
+        <h1>{title}</h1>
+        <form onSubmit={formSubmitted}>
             <label htmlFor="searchTerm">Search Term</label>
-            <input className="u-full-width" type="text" id="searchTerm" name="searchTerm" />
+            <input value={searchTerm} onChange={onSearchTermChanged} className="u-full-width" type="text" id="searchTerm" name="searchTerm" />
             <button type="submit">Search</button>
         </form>
-        <img alt="loading" id="loadingImage" src="https://imgur.com/LVHmLnb.gif" />
-        <section className="images">
+        {loading && (
+          <img alt="loading" id="loadingImage" src="https://imgur.com/LVHmLnb.gif" />
+        )}
         
+        <section className="images">
+          {images.map(url => (
+            <img key={url} alt={searchTerm} src={url} />
+          ))}
         </section>
       </React.Fragment>
   );
